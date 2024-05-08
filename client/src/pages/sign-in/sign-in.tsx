eb5d2bb8f7f12ade7de59ebfd051c4cb6eec5f75
Harsha Vardhan Konisa <67918764+harshavardhankonisa@users.noreply.github.com>
2024-05-09 01:10:23 +0530

    sign in and sign up ui ready

--- client/src/pages/sign-in/sign-in.tsx
+++ client/src/pages/sign-in/sign-in.tsx
@@ -1,9 +1,76 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const SignInPage = () => {
-    return(
-        <div>
-            This is Sign In Page
+  const [userEmail, setUserEmail] = useState<string>("");
+  const [userPassword, setUserPassword] = useState<string>("");
+
+  return (
+    <div>
+      <section className="bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full rounded-lg border shadow md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8 rounded-xl">
+              <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
+                Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="name@company.com"
+                    value={userEmail}
+                    onChange={(e)=>setUserEmail(e.target.value)}
+                    required
+                  ></input>
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    required
+                  ></input>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white bg-blue-600 hover:bg-blue-700 focus:bg-blue-950"
+                >
+                  Sign in
+                </button>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Don’t have an account yet?{" "}
+                  <Link
+                    to={'/sign-up'}
+                    className="font-medium text-blue-600 hover:underline"
+                  >
+                    Sign up
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 export default SignInPage;
--- client/src/pages/sign-up/sign-up.tsx
+++ client/src/pages/sign-up/sign-up.tsx
@@ -1,9 +1,86 @@
+import { Link } from "react-router-dom";
+
 const SignUpPage = () => {
-    return(
-        <div>
-            This is Sign Up Page
+  return (
+    <div>
+      <section className="bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
+                Create an account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required
+                  ></input>
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    required
+                  ></input>
+                </div>
+                <div>
+                  <label
+                    htmlFor="confirm-password"
+                    className="block mb-2 text-sm font-medium text-white"
+                  >
+                    Confirm password
+                  </label>
+                  <input
+                    type="confirm-password"
+                    name="confirm-password"
+                    id="confirm-password"
+                    placeholder="••••••••"
+                    className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    required
+                  ></input>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:ring-primary-800"
+                >
+                  Create an account
+                </button>
+                <p className="text-sm font-light text-gray-400">
+                  Already have an account?{" "}
+                  <Link
+                    to={'/'}
+                    className="font-medium hover:underline text-blue-500"
+                  >
+                    Login here
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 export default SignUpPage;
